547a0e3825bae01645020db7c53008f9d499f72a
Rebel1745 <31040719+Rebel1745@users.noreply.github.com>
2020-10-12 16:31:01 +0100

    Basic non-sensical movement
    Movement is a mess.  whole thing is a mess. At least I have something started

---
+++ TopDownRacer/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+    public Vector3 Offset;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Target.position + Offset;
+
+        transform.LookAt(Target);
+    }
+}
---
+++ TopDownRacer/Assets/Scripts/CarController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public float SpeedForce = 10f;
+    public float ReverseSpeedForce = -2f;
+    public float TurnTorque = -2f;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(transform.right * SpeedForce);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(transform.forward * ReverseSpeedForce);
+        }
+
+        rb.AddTorque(transform.up * Input.GetAxis("Horizontal") * TurnTorque);
+    }
+}
